2d8a782037cfa36e5da3192b75f8b6b3b1dc6c05
Karthik Kariappa <108117357+Karthikkariappa08@users.noreply.github.com>
2024-05-17 22:53:01 +0530

    server side

---
+++ Server.java
@@ -0,0 +1,19 @@
+
+
+import javax.swing.JFrame;
+
+public class Server extends JFrame{
+
+    Server(){
+        setSize(400,700);
+
+    }
+    
+    public static void main(String[]args) {
+        new Server();
+        
+    }
+
+}
+    
+
